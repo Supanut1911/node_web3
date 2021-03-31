35b36a9740a2aae65e1a5c6237bf067f3cdfa87d
Supanut Laddayam <ynotmela@gmail.com>
2021-03-31 20:56:39 +0700

    add getBalance (still bug :( )

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("Web3 Provider"));
 
+//generate account
 console.log(web3.eth.accounts.create());    
+// let acc1 = web3.eth.accounts.create()
+// console.log('>>', acc1.address);
+// const wallet = web3.eth.accounts.wallet.create(0);
+// console.log(wallet);
+
+
+web3.eth.getBalance(web3.eth.accounts.create().address, function(error, balance) {
+    if(!error) {
+        console.log( web3.utils.fromWei(balance, 'ether'));
+     } else {
+         console.log('err');
+     }
+});
+// console.log(">>>>", x);
+
+// // web3.eth.getBlockNumber().then(function(result) {
+// //     console.log(result)
+// // })
+  
